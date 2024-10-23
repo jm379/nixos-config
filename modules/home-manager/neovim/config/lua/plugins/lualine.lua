6208e3747bf2de31b41d1eea2305b507098a8cc3
Rodolfo <rodolfocavoss@gmail.com>
2024-10-23 03:21:13 -0300

    Updating lualine configs

--- modules/home-manager/neovim/config/lua/plugins/lualine.lua
+++ modules/home-manager/neovim/config/lua/plugins/lualine.lua
@@ -3,7 +3,25 @@ return {
     'nvim-lualine/lualine.nvim', 
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
-      theme = 'tokyonight'
+      theme = 'tokyonight',
+      sections = {
+        lualine_c = {
+          { 'filename', path = 4 }
+        },
+        lualine_x = { 
+          { 'filetype' },
+          {
+            'fileformat',
+            symbols = {
+              unix = '󱄅'
+            },
+            padding = {
+              left = 1,
+              right = 2
+            }
+          }
+        }
+      }
     }
   }
 }
